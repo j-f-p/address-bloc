7f4ff5b38825384f63c1e534d17a8cd0d6931af3
JF Padilla <emailreel@gmail.com>
2017-12-20 16:58:59 -0500

    enable code to handle any (non-negative) number of arguments

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,5 +1,13 @@
-greeting=ARGV[0]
-ARGV.shift
-ARGV.each do |name|
-  p "#{greeting} #{name}"
+if ARGV.length==0
+  p "Hi"
+else
+  greeting=ARGV[0]
+  if ARGV.length==1
+    p greeting
+  else
+    ARGV.shift
+    ARGV.each do |name|
+      p "#{greeting} #{name}"
+    end
+  end
 end
