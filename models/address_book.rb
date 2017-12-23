69d3736a2cafd48e570be2a484f3ce07bd088877
JF Padilla <emailreel@gmail.com>
2017-12-22 21:43:07 -0500

    add tests for method remove_entry and add that method

--- models/address_book.rb
+++ models/address_book.rb
@@ -15,4 +15,9 @@ class AddressBook
      end
      entries.insert(index, Entry.new(name, phone_number, email))
    end
+
+   def remove_entry(name, phone_number, email)
+     excess_entry = Entry.new(name, phone_number, email)
+     entries.delete(excess_entry)
+   end
 end
--- specs/address_book_spec.rb
+++ specs/address_book_spec.rb
@@ -36,4 +36,33 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "removes all entries from the address book equal to a specified entry" do
+      book = AddressBook.new
+      book.add_entry(
+        'First Last', '101.202.3030', 'first.last@email.com')
+      book.add_entry(
+        'Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry(
+        'Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry(
+        'Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+    end
+
+    it "removes the correct entry" do
+      book = AddressBook.new
+      book.add_entry(
+        'First Last', '101.202.3030', 'first.last@email.com')
+      book.add_entry(
+        'Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry(
+        'Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      first_entry = book.entries[0]
+      expect(first_entry.name).to eq('First Last')
+      expect(first_entry.phone_number).to eq('101.202.3030')
+      expect(first_entry.email).to eq('first.last@email.com')
+    end
+  end
 end
